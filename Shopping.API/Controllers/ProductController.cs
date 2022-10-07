8bdc91f90997ae4e7e3ba53dbb9b9a5416ff12f5
r4dzi0 <>
2022-10-07 13:31:57 +0200

    added shopping api project

---
+++ Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<Product> _logger;
+
+        public ProductController(ILogger<Product> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "asd"
+            })
+            .ToArray();
+        }
+    }
+}
+
